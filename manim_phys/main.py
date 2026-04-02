c133ed0d311bef1544be929b7c3b4f4e2db380cb
ettela <muyan@stu.njtc.edu.cn>
2026-04-02 20:32:32 +0800

    create `CWall` class &  manim types

--- manim_phys/main.py
+++
@@ -1,9 +0,0 @@
-from manimlib import *
-
-class TDC(Axes):
-    def construct(self):
-        return super().construct()
-class Demo(Scene):
-    def construct(self):
-        axes = TDC(x_axis_config={"lable": "a"})
-        self.add(axes)
---
+++ manim_phys/manim_types.py
@@ -0,0 +1,30 @@
+from typing import Union, Tuple, Annotated, Literal, Iterable, Dict
+from colour import Color
+import numpy as np
+import re
+
+
+type ManimColor = Union[str, Color, None]
+type RangeSpecifier = Tuple[float, float, float] | Tuple[float, float]
+
+type Span = tuple[int, int]
+type SingleSelector = Union[
+    str,
+    re.Pattern,
+    tuple[Union[int, None], Union[int, None]],
+]
+type Selector = Union[SingleSelector, Iterable[SingleSelector]]
+
+type UniformDict = Dict[str, float | bool | np.ndarray | tuple]
+
+type FloatArray = np.ndarray[int, np.dtype[np.float64]]
+type Vect2 = Annotated[FloatArray, Literal[2]]
+type Vect3 = Annotated[FloatArray, Literal[3]]
+type Vect4 = Annotated[FloatArray, Literal[4]]
+type VectN = Annotated[FloatArray, Literal["N"]]
+type Matrix3x3 = Annotated[FloatArray, Literal[3, 3]]
+type VectArray = Annotated[FloatArray, Literal["N", 1]]
+type Vect2Array = Annotated[FloatArray, Literal["N", 2]]
+type Vect3Array = Annotated[FloatArray, Literal["N", 3]]
+type Vect4Array = Annotated[FloatArray, Literal["N", 4]]
+type VectNArray = Annotated[FloatArray, Literal["N", "M"]]
